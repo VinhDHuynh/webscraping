c221b651c21ed5b67f64b25fbeed6a2cafe50dbd
Vinh Huynh <1vinhhuynh@gmail.com>
2024-06-15 18:24:36 -0400

    feat: grabbing every div by class

--- scrape.py
+++ scrape.py
@@ -1,6 +1,20 @@
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
-url ="https://webscraper.io/test-sites/e-commerce/allinone"
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-print(html)
+
+def retrieve_html():
+    url ="https://webscraper.io/test-sites/e-commerce/allinone"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    return html
+
+def main():
+    html = retrieve_html()
+    print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    cards = soup.find_all("div", {"class":"product-wrapper card-body"})
+    print(cards)
+
+
+if __name__ == "__main__":
+    main()
