8e8193dc43d959e4773dd085e12b56e052faa503
Vinh Huynh <1vinhhuynh@gmail.com>
2024-06-16 16:20:24 -0400

    feat: extract ratings list

--- scrape.py
+++ scrape.py
@@ -1,19 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import re
 
-def retrieve_html():
+def get_html():
     url ="https://webscraper.io/test-sites/e-commerce/allinone"
     page = urlopen(url)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
     return html
 
+def get_ratings(soup,cards):
+    # find all pg tags with a data-rating attribute
+    ratings_html = soup.find_all("p", {"data-rating" : re.compile(r".*")}) 
+    ratings = []
+    for x in ratings_html:
+        ratings.append(x["data-rating"])
+# Parse things about each item and put it in a csv
+# h4 class="price float-end card-title pull-right" for costs
+# p  data-rating="4" 
 def main():
-    html = retrieve_html()
-    print(html)
+    html = get_html()
     soup = BeautifulSoup(html, 'html.parser')
     cards = soup.find_all("div", {"class":"product-wrapper card-body"})
-    print(cards)
+    ratings = get_ratings(soup, cards)
 
 
 if __name__ == "__main__":
